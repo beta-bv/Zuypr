e0d1ca7428dfc4b3f0120b417fd10ffe5a83bc6e
Siem Gerritsen <siem2004gerritsen@gmail.com>
2022-11-30 16:46:44 +0100

    Finished getters and setters of User. and added summaries

--- Model/User.cs
+++ Model/User.cs
@@ -9,34 +9,43 @@ namespace Model
         private string _name;
         private string _email;
         private DateTime _dateOfBirth;
-        public string Name {
-            get { 
-                return _name; 
-            }
-            set {
-                if(!IsNameValid(value))
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (!IsNameValid(value))
                 {
                     throw new Exception("NAME INCORRECT");
                 }
                 _name = value;
-            } 
             }
-        public string Email {
-            get { return _email; } 
-            set {
-                if(!new EmailAddressAttribute().IsValid(value)) 
+        }
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                if (!new EmailAddressAttribute().IsValid(value))
                 {
                     throw new Exception("EMAIL INVALID");
                 }
                 _email = value;
-            } }
-        public DateTime DateOfBirth { get{ return _dateOfBirth; }
-            set {
-            if(!CheckAge(value) || !IsDateOfBirthValid(value))
+            }
+        }
+        public DateTime DateOfBirth
+        {
+            get { return _dateOfBirth; }
+            set
+            {
+                if (!CheckAge(value) || !IsDateOfBirthValid(value))
                 {
                     throw new Exception("INVALID BIRTH DATE");
                 }
-            } }
+                _dateOfBirth = value;
+            }
+        }
         public List<string> Cities { get; set; }
         public Bitmap ProfielImage { get; set; }
         public List<User> Matches { get; set; }
@@ -51,34 +60,50 @@ namespace Model
             DateOfBirth = dateOfBirth;
         }
 
-        public static bool CheckAge(DateTime date) //checkt of iemand wel 18 is
+        /// <summary>
+        /// Checks the age to be 18 or older.
+        /// </summary>
+        /// <param name="dateOfBirth">The date of birth</param>
+        /// <returns></returns>
+        public static bool CheckAge(DateTime dateOfBirth)
         {
             DateTime dateNow = DateTime.Now;
 
-            if (date.AddYears(18).CompareTo(dateNow) > 0)
-            {
-            return false;
-            }
-            return true;
-        }
-
-        public static bool IsNameValid(string name)// checkt of de naam geen aparte tekens bevat en niet langer is dan 50 en niet korter dan 1
-        {
-            name = name.Trim();
-            return (name.Length > 0 && name.Length <= 50 && name.All(char.IsLetter));
-        }
-
-        public static bool IsDateOfBirthValid(DateTime date)// checked of de datum die wordt ingevuld niet in de toekomst is.
-        {
-            DateTime dateNow = DateTime.Now;
-            if (date > dateNow)
+            if (dateOfBirth.AddYears(18).CompareTo(dateNow) > 0)
             {
                 return false;
             }
             return true;
         }
 
-        public User GetDummyUser() //returned een dummy user voor dummy gebruik
+        /// <summary>
+        /// Checks if a name is not empty or longer than 50 characters and only allows letters and spaces.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public static bool IsNameValid(string name)
+        {
+            name = name.Trim();
+            return (name.Length > 0 && name.Length <= 50 && name.All(a => char.IsLetter(a) || a == ' '));
+        }
+
+        /// <summary>
+        /// Verifies that the given date is not in the future.
+        /// </summary>
+        /// <param name="date"></param>
+        /// <returns></returns>
+        public static bool IsDateOfBirthValid(DateTime date)
+        {
+            DateTime dateNow = DateTime.Now;
+
+            return date < dateNow;
+        }
+
+        /// <summary>
+        /// Creates a dummy user
+        /// </summary>
+        /// <returns></returns>
+        public User GetDummyUser()
         {
             return new User("dummyUser", "email@email.com", new DateTime(1, 1, 1999));
         }
