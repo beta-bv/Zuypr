0d6c1aabcb299b0f8b170bc861fa23410167e370
Siem Gerritsen <siem2004gerritsen@gmail.com>
2022-11-30 10:28:04 +0100

    Added Drink class

---
+++ Zuypr/Model/Drink.cs
@@ -0,0 +1,24 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public enum DrinkType
+    {
+        Beer,
+        Wine,
+        Liquor
+    }
+    public class Drink
+    {
+        public Bitmap DrinkImage { get; set; }
+        public DrinkType DrinkType { get; set;}
+        public string DrinkName { get; set; }
+        public float Percentage { get; set; }
+    }
+}
