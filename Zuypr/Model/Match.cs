a2a3008ffc8f2c68f304e6925475bc5734215b62
Siem Gerritsen <siem2004gerritsen@gmail.com>
2022-11-30 10:41:37 +0100

    Added Match, Chat and Message classes

---
+++ Zuypr/Model/Match.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Match
+    {
+        public User[] Users { get; set; }
+        public Chat Chat { get; set; }
+    }
+}
