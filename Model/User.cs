2ddbf6d9ff23150c3c7c2552c2e37d21634368e9
slijfergast <slijfergast@gmail.com>
2022-11-30 12:26:45 +0100

    effe runable gemaakt

--- Model/User.cs
+++ Model/User.cs
@@ -7,7 +7,7 @@ namespace Model
     {
         public string Name { get; set; }
         public string Email { get; set; }
-        public DateOnly DateOfBirth { get; set; }
+        public DateTime DateOfBirth { get; set; }
         public List<string> Cities { get; set; }
         public Bitmap ProfielImage { get; set; }
         public List<User> Matches { get; set; }
@@ -15,26 +15,32 @@ namespace Model
         public List<Drink> Likes { get; set; }
         public List<Drink> Dislikes { get; set; }
 
-        public User(string name, string email, DateOnly dateOfBirth){
+        public User(string name, string email, DateTime dateOfBirth){
             Name = name;
             Email = email;
             DateOfBirth = dateOfBirth;
         }
-        public bool CheckDateOfBirth(DateOnly date){
-            DateOnly dateNow = DateOnly.FromDateTime(DateTime.Now);
+        public bool CheckAge(DateTime date){
+            DateTime dateNow = DateTime.Now;
 
             if(date.CompareTo(dateNow) < 18)
             {
                 return false;
             }
-        return true;
+            return true;
         }
-        public bool CheckEmail(string email) { }
-        public bool CheckName(string name) { }
+
+        public bool CheckEmail(string email) {
+            return true;
+        }
+
+        public bool CheckName(string name) {
+            return true;
+        }
 
         public User GetDummyUser() //returned een dummy user voor dummy gebruik
         {
-            return new User("dummyUser", "email@email.com", new DateOnly(1, 1, 1999) );
+            return new User("dummyUser", "email@email.com", new DateTime(1, 1, 1999) );
         }
 
 
