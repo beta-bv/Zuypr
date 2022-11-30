cb621888d6681d122428b34425438e9545c10ff1
slijfergast <slijfergast@gmail.com>
2022-11-30 15:27:32 +0100

    fix werkt nog niet helemaal maar gaat wel hard

--- Model/User.cs
+++ Model/User.cs
@@ -4,10 +4,22 @@ using System.Drawing;
 
 namespace Model
 {
-    // All the code in this file is included in all platforms.
     public class User
     {
-        public string Name { get; set; }
+        private string _name;
+        public string Name {
+            get { 
+                return _name; 
+            }
+            set {
+                if(!IsNameValid(value))
+                {
+                    throw new Exception("SCHRIJF JE KUT NAMEN GOED KUTHOERTJE IK KOM JE MOEDER NEUKEN (:");
+                }
+                _name = value;
+
+            } 
+            }
         public string Email { get; set; }
         public DateTime DateOfBirth { get; set; }
         public List<string> Cities { get; set; }
@@ -24,7 +36,7 @@ namespace Model
             DateOfBirth = dateOfBirth;
         }
 
-        public static bool CheckAge(DateTime date)
+        public static bool CheckAge(DateTime date) //checkt of iemand wel 18 is
         {
             DateTime dateNow = DateTime.Now;
 
@@ -35,16 +47,16 @@ namespace Model
             return true;
         }
 
-        public static bool IsNameValid(string name)
+        public static bool IsNameValid(string name)// checkt of de naam geen aparte tekens bevat en niet langer is dan 50 en niet korter dan 1
         {
             name = name.Trim();
             return (name.Length > 0 && name.Length <= 50 && name.All(char.IsLetter));
         }
 
-        public static bool IsDateOfBirthValid(DateTime date)
+        public static bool IsDateOfBirthValid(DateTime date)// checked of de datum die wordt ingevuld niet in de toekomst is.
         {
             DateTime dateNow = DateTime.Now;
-            if(date > dateNow)
+            if (date > dateNow)
             {
                 return false;
             }
---
+++ Tests/User_SettersShould.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Tests
+{
+    public class User_SettersShould
+    {
+        public User_SettersShould()
+        {
+            User TestUser = new User("crong", "gmail@gmail.com", new DateTime(2000, 10, 2));
+        }
+
+        [Theory]
+        [InlineData("cringe", true)]
+        [InlineData("......,,,///////\\\\", false)]
+        public void CheckName(string name, bool Expected)
+        {
+            Assert.Equal(Expected, TestUser.Name = name);
+        }
+        [Theory]
+        [InlineData(2010, 1, 1, false)]
+        [InlineData(1999, 1, 1, true)]
+        public void CheckAge(int year, int month, int day, bool Expected)
+        {
+            DateTime date = new DateTime(year, month, day);
+            Assert.Equal(Expected, User.DateOfBirth = date);
+        }
+        [Theory]
+        [InlineData("cringe", false)]
+        [InlineData("example@gmail.com", true)]
+        public void CheckEmail(string name, bool Expected)
+        {
+            Assert.Equal(Expected, User.Email = name);
+        }
+
+    }
+}
