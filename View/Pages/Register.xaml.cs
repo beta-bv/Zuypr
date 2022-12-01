3a7275eeb0be36bcfac1d0b99ee8dcb235924300
slijfergast <slijfergast@gmail.com>
2022-12-01 14:08:37 +0100

    eerste werk ding

--- View/Pages/Register.xaml.cs
+++ View/Pages/Register.xaml.cs
@@ -1,10 +1,28 @@
-﻿namespace View.Pages;
+﻿using Model;
+
+namespace View.Pages;
 
 public partial class Register : ContentPage
 {
     public Register()
     {
         InitializeComponent();
+    }
+    private async void RegisterUser(object sender, EventArgs e)
+    {
+        string name = NameField.Text.Trim();
+        string email = EmailField.Text.Trim();
+        DateTime dateOfBirth = BirthDateField.Date;
+        string password = "";
+        if(User.ComparePasswords(PasswordField.Text, PasswordConfirmField.Text))
+        {
+            password = User.HashString(PasswordField.Text);
+            User Client = new User(name, email, password, dateOfBirth);
+        }
+        else
+        {
+            new Label() { Text = "de wachtwoorden kwamen niet overeen" };
+        }
     }
 
 }
