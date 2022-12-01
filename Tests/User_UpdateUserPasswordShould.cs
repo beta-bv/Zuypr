7ff4a4b84564c54b0ccd0e22ccc6c0fd58fac8af
slijfergast <slijfergast@gmail.com>
2022-12-01 12:00:26 +0100

    tests voor update password

--- Tests/User_UpdateUserPasswordShould.cs
+++ Tests/User_UpdateUserPasswordShould.cs
@@ -12,14 +12,22 @@ namespace Tests
         User TestUser;
         public User_UpdateUserPasswordShould()
         {
-            TestUser = new User("crong", "gmail@gmail.com", "123", new DateTime(2000, 10, 2));
+            TestUser = new User("crong", "gmail@gmail.com", "KrakendeBanden#", new DateTime(2000, 10, 2));
         }
 
         [Theory]
-        [InlineData("", false)]
-        public void CheckName(string name, bool Expected)
+        [InlineData("KrakendeBanden#", "123", "123", false)]
+        [InlineData("KrakendeBanden#", "123", "12", false)]
+        [InlineData("12", "124", "124", false)]
+        [InlineData("KrakendeBanden#", "Niw", "", false)]
+        [InlineData("", "", "", false)]
+        [InlineData("KrakendeBanden#", "VageJonges", "VageJonges", false)]
+        [InlineData("KrakendeBanden#", "vageJonges*", "vageJonges*", false)]
+        [InlineData("KrakendeBanden#", "VageJonges*", "VageJonges*", true)]
+        [InlineData("KrakendeBanden#", "GoedWachtWoordman!", "GoedWachtWoordman!", true)]
+        public void CheckName(string passwordOld, string passwordNew1, string passwordNew2, bool Expected)
         {
-            Assert.Equal(Expected, User.IsNameValid(name));
+            Assert.Equal(Expected, TestUser.UpdateUserPassword(passwordOld, passwordNew1, passwordNew2));
         }
     }
 }
