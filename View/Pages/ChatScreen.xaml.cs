528213ea8a668b13db4a3dd8454e1126d0ab0b18
slijfergast <slijfergast@gmail.com>
2022-12-05 16:10:52 +0100

    Send stock image go brrrrr
    Wilde een image button doen ipv normale button, nog niet gelukt. Maar ik ben wel bezig met de extra vormgeving van de chat messages

--- View/Pages/ChatScreen.xaml.cs
+++ View/Pages/ChatScreen.xaml.cs
@@ -1,4 +1,6 @@
 ﻿namespace View.Pages;
+
+using Microsoft.Maui.Controls.Shapes;
 using Model;
 
 public partial class ChatScreen : ContentPage
@@ -12,7 +14,20 @@ public partial class ChatScreen : ContentPage
     {
         String messageToSend = chatbox.Text?.Trim();
         //Message Message = new Message(messageToSend,User.GetDummyUser(), DateTime.Now);
-        ChatMessageView.Children.Add(new Label { Text = messageToSend});
-
+        ChatMessageView.Children.Add(new Border {
+            Stroke = Color.FromArgb("#FFFFFF"),
+            Background = Color.FromArgb("#008000"),
+            StrokeThickness = 4,
+            Padding = new Thickness(16, 8),
+            HorizontalOptions = LayoutOptions.Start,
+            StrokeShape = new Rectangle,
+            Content = new Label
+            {
+                Text = messageToSend,
+                TextColor = Colors.White,
+                FontSize = 18,
+                FontAttributes = FontAttributes.Bold
+            }
+        }) ;
     }
 }
