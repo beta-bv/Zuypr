5ac107a7661d72a01b11d0f280bf2fd58b72b31d
markvklompenburg <81861849+markvklompenburg@users.noreply.github.com>
2022-12-06 10:29:22 +0100

    Indeling goed

--- Model/Drink.cs
+++ Model/Drink.cs
@@ -14,16 +14,24 @@ namespace Model
         Wine,
         Liquor
     }
+
     public class Drink
     {
-        public Bitmap DrinkImage { get; set; }
+        public string DrinkImage { get; set; }
         public DrinkType DrinkType { get; set;}
         public string DrinkName { get; set; }
         public float Percentage { get; set; }
 
+        public Drink(string drinkImage, DrinkType drinkType, string drinkName, float percentage) {
+            DrinkImage = drinkImage;
+            DrinkType = drinkType;
+            DrinkName = drinkName;
+            Percentage = percentage;
+        }
+
         public static Drink GetDummyDrink()
         {
-            return new Drink();
+            return new Drink("https://static.ah.nl/dam/product/AHI_43545239363438323836?revLabel=1&rendition=800x800_JPG_Q90&fileType=binary", DrinkType.Beer, "Beer", 10);
         }
     }
 
--- View/Pages/MyDrinks.xaml.cs
+++ View/Pages/MyDrinks.xaml.cs
@@ -1,9 +1,36 @@
-﻿namespace View.Pages;
+﻿using Model;
+namespace View.Pages;
 
 public partial class MyDrinks : ContentPage
 {
+    public List<Drink> AllDrinks { get; private set; }
+    public string Test = "test";
+
     public MyDrinks()
     {
+        AllDrinks = new List<Drink>();
+        AllDrinks.Add(Drink.GetDummyDrink());
+        AllDrinks.Add(Drink.GetDummyDrink());
+        AllDrinks.Add(Drink.GetDummyDrink());
+        AllDrinks.Add(Drink.GetDummyDrink());
+        AllDrinks.Add(Drink.GetDummyDrink());
+        AllDrinks.Add(Drink.GetDummyDrink());
         InitializeComponent();
+        BindingContext = this;
+    }
+
+    async void OnButtonFavoriteClicked(object sender, EventArgs args)
+    {
+        AllDrinks.Add(Drink.GetDummyDrink());
+    }
+
+    async void OnButtonLikeClicked(object sender, EventArgs args)
+    {
+        AllDrinks.Add(Drink.GetDummyDrink());
+    }
+
+    async void OnButtonDislikeClicked(object sender, EventArgs args)
+    {
+        AllDrinks.Add(Drink.GetDummyDrink());
     }
 }
