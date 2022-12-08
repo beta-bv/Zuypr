bd2e0b8533193e492873dfe5cd78c514c187444b
StanLipman <104895080+StanLipman@users.noreply.github.com>
2022-12-08 09:28:11 +0100

    Added stuff to profile page

--- View/Pages/Profile.xaml.cs
+++ View/Pages/Profile.xaml.cs
@@ -9,7 +9,22 @@ public partial class Profile : ContentPage
     public Profile()
     {
         MatchUser = new User("Henk", "hoi@gmail.com", "Neeneenee!", new DateTime(2002, 7, 15));
+        MatchUser.Cities = new List<string>();
+        MatchUser.Cities.Add("Hoonhorst");
+        MatchUser.Cities.Add("Dalfsen");
+
+        Drink testDrink = Drink.GetDummyDrink();
+
+        Drink testDrink2 = Drink.GetDummyDrink();
+
+        MatchUser.AddToFavourites(testDrink);
+        MatchUser.AddToFavourites(testDrink2);
+
         InitializeComponent();
+        if (MatchUser.Cities.Count < 2)
+        {
+            city.Text = "City";
+        }
         OwnProfile = true;
         if (OwnProfile)
         {
@@ -21,8 +36,12 @@ public partial class Profile : ContentPage
     public Profile(User user)
     {
         MatchUser = user;
-       
+        MatchUser.Cities = new List<string>();
         InitializeComponent();
+        if (MatchUser.Cities.Count < 2)
+        {
+            city.Text = "City";
+        }
         if (OwnProfile)
         {
             chat.IsVisible = false;
