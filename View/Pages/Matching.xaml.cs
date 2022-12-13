fbd91938a67e096400b9b20419eb4838f15a6af4
luximus-hunter <thomaslipman10@gmail.com>
2022-12-13 10:35:58 +0100

    ..

--- View/Pages/Matching.xaml.cs
+++ View/Pages/Matching.xaml.cs
@@ -1,9 +1,14 @@
-﻿namespace View.Pages;
+﻿using Model;
+
+namespace View.Pages;
 
 public partial class Matching : ContentPage
 {
+    public User Match { get; }
+
     public Matching()
     {
+        Match = Controller.Auth.getUser();
         InitializeComponent();
     }
 }
