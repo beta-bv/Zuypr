7531ac611d6acd3a78965f37ef7ae5227e56e6be
slijfergast <slijfergast@gmail.com>
2022-12-14 10:10:14 +0100

    grotedeels werkende logica van veranderen van email

--- View/Pages/Settings.xaml.cs
+++ View/Pages/Settings.xaml.cs
@@ -1,16 +1,37 @@
-﻿namespace View.Pages;
+﻿using Controller;
+using Model;
+using System.Linq.Expressions;
+
+namespace View.Pages;
 
 public partial class Settings : ContentPage
 {
     public Settings()
     {
         InitializeComponent();
+        EmailEditOld.Text = Auth.getUser().Email;
     }
-    
+
     private void Logout(object sender, EventArgs e)
     {
         if (Application.Current != null) Application.Current.MainPage = new LaunchScreen();
+
+    }
+
+    private void OnEditClicked(object sender, EventArgs e)
+    {
+        try
+        {
+            User temp = Auth.getUser();                  //geeft alleen nog geen error message bij het invullen van niks
+            temp.Email = EmailEditNew.Text?.Trim();
+            ErrorFrameEditPage.IsVisible = false;
+            Auth.setUser(temp);
+        }
         
-        
+        catch (Exception ex)
+        {
+            ErrorLabelEditPage.Text = ex.Message;
+            ErrorFrameEditPage.IsVisible = true;
+        }
     }
 }
