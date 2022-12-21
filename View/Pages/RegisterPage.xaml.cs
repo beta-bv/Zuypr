f3897e375bb522f88f71610ee947a147ee79f88b
slijfergast <slijfergast@gmail.com>
2022-12-21 13:22:49 +0100

    早上好中国 现在我有冰淇淋 我很喜欢冰淇淋 但是 速度与激情9 比冰淇淋 速度与激情 速度与激情9 我最喜欢 所以…现在是音乐时间 准备 1 2 3 两个礼拜以后 速度与激情9 ×3 不要忘记 不要错过 记得去电影院看速度与激情9 因为非常好电影 动作非常好 差不多一样冰淇淋 再见

--- View/Pages/RegisterPage.xaml.cs
+++ View/Pages/RegisterPage.xaml.cs
@@ -2,14 +2,18 @@
 using System.Linq.Expressions;
 using Controller;
 using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Data.SqlClient;
 
 namespace View.Pages;
 
 public partial class RegisterPage : ContentPage
 {
+    private DatabaseContext _db;
     public RegisterPage()
     {
         InitializeComponent();
+        _db = new DatabaseContext();
     }
     /// <summary>
     ///  maakt de user aan
@@ -31,8 +35,27 @@ public partial class RegisterPage : ContentPage
                 try
                 {
                     User Client = new User(name, email, password, dateOfBirth);
-                    Model.Database.DB.Add(Client);
-                    Model.Database.DB.SaveChanges();
+                    using (SqlConnection connection = new SqlConnection("Data Source=127.0.0.1;Initial Catalog=zuypr;User ID=sa;Password=Betaverse01!;Trust Server Certificate=True"))
+                    {
+                        String query = "INSERT INTO dbo.users (id,Name,Email,Password, DateOfBirth, ProfielImage) VALUES (@id,@Name,@Email,@Password, @DateOfBirth, ProfielImage)";
+
+                        using (SqlCommand command = new SqlCommand(query, connection))
+                        {
+                            command.Parameters.AddWithValue("@Id", "2");
+                            command.Parameters.AddWithValue("@Name", name);
+                            command.Parameters.AddWithValue("@Email", email);
+                            command.Parameters.AddWithValue("@Password", password);
+                            command.Parameters.AddWithValue("@DateOfBirth", dateOfBirth);
+                            command.Parameters.AddWithValue("@ProfielImage", "dotnet_bot.png");
+
+                            connection.Open();
+                            int result = command.ExecuteNonQuery();
+
+                            // Check Error
+                            if (result < 0)
+                                throw new Exception("Error inserting data into Database!");
+                        }
+                    }
                     Auth.User = Client;
                     Application.Current.MainPage = new AppShell(Client);
                 }
