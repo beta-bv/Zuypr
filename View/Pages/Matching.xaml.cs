cca8bf6d36ad83a95caf51f3fc90f03513a2bbda
markvklompenburg <81861849+markvklompenburg@users.noreply.github.com>
2022-12-21 14:26:27 +0100

    Korterefunctie

--- View/Pages/Matching.xaml.cs
+++ View/Pages/Matching.xaml.cs
@@ -10,36 +10,15 @@ public partial class Matching : ContentPage
     //Dit wordt straks gekoppeld aan de lijst met potentiele matches die uit het algoritme komen (Input filter).
     public List<User> Users => dummydb.Users;
 
-    //Een lijst met potentiele matches die zijn gefilterd op plaats/dorp.
-    public List<User> FilterPlace { get; set; } = new List<User>();
-
     //De uiteindelijke lijst potentiele matches die zijn gefilterd op plaats/dorp en leeftijd (Output filter).
-    public List<User> FilteredPotentionalMatches => FilterPlace.Where(x => x.Age >= user.MinimalAge).Where(x => x.Age <= user.MaximalAge).ToList();
-
+    public List<User> FilteredPotentionalMatches => Users.Where(
+                                                        a => user.Cities.Intersect(a.Cities).Count() > 0)
+                                                        .Where(x => x.Age >= user.MinimalAge)
+                                                        .Where(x => x.Age <= user.MaximalAge)
+                                                        .ToList();
     public Matching()
     {
         InitializeComponent();
-        FilterCitys();
         BindingContext = this;
-    }
-
-    /// <summary>
-    /// Filters potential matches that have selected the same citys
-    /// </summary>
-    public void FilterCitys()
-    {
-        foreach (User userMatch in Users)
-        {
-            foreach (City city in userMatch.Cities)
-            {
-                foreach (City cityMe in user.Cities)
-                {
-                    if (city.Name.Equals(cityMe.Name) && !FilterPlace.Contains(userMatch))
-                    {
-                        FilterPlace.Add(userMatch);
-                    }
-                }
-            }
-        }
     }
 }
