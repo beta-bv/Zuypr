d8c57cd9ae0b20e1b22cb9f25909d215fb7dc061
markvklompenburg <81861849+markvklompenburg@users.noreply.github.com>
2022-12-21 14:59:14 +0100

    MVC

--- View/Pages/Matching.xaml.cs
+++ View/Pages/Matching.xaml.cs
@@ -5,17 +5,8 @@ using System.Linq;
 
 public partial class Matching : ContentPage
 {
-    readonly static User user = Auth.getUser();
+    public static List<User> PotentionalMatches => Filter.FilteredPotentionalMatches;
 
-    //Dit wordt straks gekoppeld aan de lijst met potentiele matches die uit het algoritme komen (Input filter).
-    public List<User> Users => dummydb.Users;
-
-    //De uiteindelijke lijst potentiele matches die zijn gefilterd op plaats/dorp en leeftijd (Output filter).
-    public List<User> FilteredPotentionalMatches => Users.Where(
-                                                        a => user.Cities.Intersect(a.Cities).Count() > 0)
-                                                        .Where(x => x.Age >= user.MinimalAge)
-                                                        .Where(x => x.Age <= user.MaximalAge)
-                                                        .ToList();
     public Matching()
     {
         InitializeComponent();
