e3adb14b3c5f188abcc3abcf125a64ba1c237512
markvklompenburg <81861849+markvklompenburg@users.noreply.github.com>
2022-12-21 15:00:53 +0100

    fix

--- View/Pages/Matching.xaml.cs
+++ View/Pages/Matching.xaml.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 public partial class Matching : ContentPage
 {
-    public static List<User> PotentionalMatches => Filter.FilteredPotentionalMatches;
+    public List<User> PotentionalMatches => Filter.FilteredPotentionalMatches;
 
     public Matching()
     {
