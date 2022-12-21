3a5aaa2faf3841f2c24db8a5fa97591656c2ec94
luximus-hunter <thomaslipman10@gmail.com>
2022-12-21 09:50:09 +0100

    ..

--- Model/Model.cs
+++ Model/Model.cs
@@ -5,8 +5,6 @@ namespace Model;
 
 public class DatabaseContext : DbContext
 {
-    public static DatabaseContext Database { get; } = new();
-
     public DbSet<Bar> Bars { get; set; }
     public DbSet<Drink> Drinks { get; set; }
     public DbSet<Location> Locations { get; set; }
