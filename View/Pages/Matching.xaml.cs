de4413bc31f8112778708f688225d7a6e12502d7
markvklompenburg <81861849+markvklompenburg@users.noreply.github.com>
2022-12-22 13:51:31 +0100

    Bam fix

--- View/Pages/Matching.xaml.cs
+++ View/Pages/Matching.xaml.cs
@@ -1,111 +1,73 @@
 ﻿using Model;
 using Controller;
 using System.Text.RegularExpressions;
+using System.Collections.Generic;
 //using static Java.Util.Jar.Attributes;
 
 namespace View.Pages;
 
 public partial class Matching : ContentPage
 {
+    public static List<User> Users { get; set; } = dummydb.Users;
+    public static Queue<User> UsersQue = new Queue<User>(Users);
     public User Match { get; set; }
+
     public List<Drink> Drinks { get; set; }
-    public List<User> Users { get; set; }
     public List<Message> messages { get; set; } 
     public User[] MatchUsers { get; set; } = new User[2];
     public bool YesBool = false;
+
     public Matching()
     {
-        InitializeComponent();
-        Users = dummydb.Users;
-        Users.Insert(0, Auth.getUser());
-        NextUser(Users);
-    }
-
-    public Matching(List<User> users) {
-        Users = users;
-        Match = Users[0];
-        Drinks = Match.GetFavourites();
-        InitializeComponent();
-        if (Users is null || Users.Count() == 0)
+        if (UsersQue.Count() == 0)
         {
             NoMatches.IsVisible = true;
         }
-        if (Drinks.Count < 2)
-        {
-            favorite.Text = "Favourite beverage:";
-        }
+
+        Match = UsersQue.Dequeue();
+
+        InitializeComponent();
         BindingContext = this;
     }
 
-    private void NextUser(List<User> users) {
-        users.RemoveAt(0);
-        Application.Current.MainPage = new Matching(users);
-        Users = users;
-        Match = users[0];
-        Drinks = Match.GetFavourites();
-        Thread.Sleep(100);
-        if (Drinks.Count < 2)
+    private void NextUser() {
+
+        if (UsersQue.Count() > 0)
         {
-            favorite.Text = "Favorite beverage:";
+            //Changes the labels on the screen to the current user
+            Match = UsersQue.Dequeue();
+            MatchName.Text = Match.Name;
+            MatchImage.Source = Match.ProfileImage;
+
+            //Changes the labels on the MsgAndBackPopUp
+            MatchFoundImage.Source = Match.ProfileImage;
+            MatchFoundName.Text = Match.Name;
         }
-        //if (YesBool) {
-        //    MsgAndBackPopUp.IsVisible = true;
-        //}
-        if (Users is null || Users.Count() == 1)
-        {
+        else {
             NoMatches.IsVisible = true;
         }
-        YesBool = false;
-        BindingContext = this;
     }
-
 
     private void Yes_Clicked(object sender, EventArgs e)
     {
-        //if (MatchAdded())
-        //{
-            var temp = (Button)sender;
-            // Show next person
-            // If match show Profile card and chat button
-            // Update List
-            // Make Match
-            Model.Match NewMatch = new Model.Match(new User[] { Auth.getUser(), Match }, new List<Message>());
-            Auth.getUser().Matches.Add(NewMatch);
-            Match.Matches.Add(NewMatch);
-            MsgAndBackPopUp.IsVisible = true;
-            //Array.Clear(MatchUsers, 0, MatchUsers.Length);
-            // Set Database Match and Chat
-            if (Users is null || Users.Count() == 1)
-            {
-                NoMatches.IsVisible = true;
-            }
-            else
-            {
-                YesBool = true;
-            }
-        // }
+        //Adds a match to the users.
+        Model.Match NewMatch = new Model.Match(new User[] { Auth.getUser(), Match }, new List<Message>());
+        Auth.getUser().Matches.Add(NewMatch);
+        Match.Matches.Add(NewMatch);
+
+        MsgAndBackPopUp.IsVisible = true;
     }
 
     private void No_Clicked(object sender, EventArgs e)
     {
-        
-        var temp = (Button)sender;
-        if (Users is null || Users.Count() == 1)
-        {
-            NoMatches.IsVisible= true;
-        }
-        else
-        {
-            NextUser(Users);
-        }
-
-        // Show next person
-        // Update List
+        NextUser();
     }
+
     private void Back_Clicked(object sender, EventArgs e){
         MsgAndBackPopUp.IsVisible = false;
-        NextUser(Users);
+        NextUser();
     }
+
     private void Message_Clicked(object sender, EventArgs e) {
         foreach (Model.Match match in Controller.Auth.getUser().Matches)
         {
@@ -118,9 +80,5 @@ public partial class Matching : ContentPage
             }
         }
     }
-    public bool MatchAdded()
-    {
-        return true;
-        //throw new Exception("matchadded is nog niet implemented");
-    }
+
 }
