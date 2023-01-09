26b746be4e26549984f0251a2428f254b3d79707
slijfergast <slijfergast@gmail.com>
2023-01-09 20:13:48 +0100

    efwaffd

--- View/App.xaml.cs
+++ View/App.xaml.cs
@@ -9,6 +9,19 @@ public partial class App : Application
     {
         InitializeComponent();
         //dummydb.Initialize();
+        try
+        {
+            //User mark = UserDatabaseOperations.GetUserFromDatabaseByEmail("siem@siemgerritsen.nl");
+            //User merijn = UserDatabaseOperations.GetUserFromDatabaseByEmail("stan@email.nl");
+            //DatabaseContext db = new DatabaseContext();
+            //Match CurrentMatch = new Match();
+            //merijn.Matches.Add(CurrentMatch);
+            //mark.Matches.Add(CurrentMatch);
+            //UserDatabaseOperations.UpdateUserInDatabase(mark);
+            //UserDatabaseOperations.UpdateUserInDatabase(merijn);
+        }
+        catch (Exception E) { }
+
         Model.City.GetValidCities();
         MainPage = new LaunchScreen();
     }
