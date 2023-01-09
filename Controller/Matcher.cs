619bc301387efefb9d2058c1d8c9819b1f14149a
slijfergast <slijfergast@gmail.com>
2023-01-09 17:35:33 +0100

    llllllllll

--- Controller/Matcher.cs
+++ Controller/Matcher.cs
@@ -92,7 +92,12 @@ namespace Controller
             }
             // Excluding the logged-in user from all users
             // HACK: Should stil be fixed for official DB
-            UserList = dummydb.Users.Where(u => u.Name != User.Name).ToList();
+            try
+            {
+                DatabaseContext db = new DatabaseContext();
+                UserList = db.Users.Where(u => u.Name != User.Name).ToList();
+            }
+            catch (Exception ex) { }
 
             MyFavouriteDrinks = User.GetFavourites().Select(d => d.Name).ToArray();
             MyLikedDrinks = User.GetLikes().Select(d => d.Name).ToArray();
