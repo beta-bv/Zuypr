25f916f4d6461dea26eafb0d09b34d9c1bb5c8eb
Paradera <d.k.1997@hotmail.com>
2023-01-11 22:05:21 +0100

    Fuck dit

--- dummy/Program.cs
+++
@@ -1,4 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine(".NET MAUI is een bitch dus dit apparte project is nodig om EF werkend te krijgen..");
-Console.WriteLine("Thanks Microsoft!");
-Console.ReadLine();
